68dc7bb4224df892c2097f85040ec73751608490
Currency-Converter <z8784507@gmail.com>
2026-04-06 21:13:10 +0500

    editing authentication

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -30,7 +30,11 @@ export const signin = async (req, res, next) => {
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
     const { password: pass, ...rest } = validUser._doc;
     res
-      .cookie("access_token", token, { httpOnly: true })
+      .cookie("access_token", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+      })
       .status(200)
       .json(rest);
   } catch (error) {
@@ -45,7 +49,11 @@ export const google = async (req, res, next) => {
       const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
       const { password: pass, ...rest } = user._doc;
       res
-        .cookie("access_token", token, { httpOnly: true })
+        .cookie("access_token", token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: "none",
+        })
         .status(200)
         .json(rest);
     } else {
@@ -55,25 +63,33 @@ export const google = async (req, res, next) => {
       const newUser = new User({
         username:
           req.body.name.split(" ").join("").toLowerCase() +
-          Math.random.toString(36).slice(-4),  
+          Math.random.toString(36).slice(-4),
         email: req.body.email,
-        password: hashedPassword, avatar: req.body.photo
+        password: hashedPassword,
+        avatar: req.body.photo,
       });
       await newUser.save();
-      const token = jwt.sign({id: newUser._id}, process.env.JWT_SECRET);
-      const {password: pass, ...rest}= newUser._doc;
-      res.cookie("access_token", token, {httpOnly: true}).status(200).json(rest);
+      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+      const { password: pass, ...rest } = newUser._doc;
+      res
+        .cookie("access_token", token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: "none",
+        })
+        .status(200)
+        .json(rest);
     }
   } catch (error) {
     next(error);
   }
 };
 
-export const signout =async (req, res, next) =>{
+export const signout = async (req, res, next) => {
   try {
     res.clearCookie("access_token");
     res.status(200).json("User has been logged out!");
   } catch (error) {
     next(error);
   }
-}
+};
