7ccbef6602624d7ab51e1d8080231bcd02429a0c
Currency-Converter <z8784507@gmail.com>
2026-04-06 16:17:33 +0500

    Updation for removing error

--- index.js
+++ index.js
@@ -45,6 +45,8 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
+export default app;
